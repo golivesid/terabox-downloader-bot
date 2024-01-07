6b8fa87b59d07908e4a6c151d476bde3f6e0b95c
golivesid <155578467+golivesid@users.noreply.github.com>
2024-01-07 14:34:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,37 @@
-api_id =  #my.telegram.org
-api_hash = "" #my.telegram.org
+api_id = 23054736  #my.telegram.org
+api_hash = "d538c2e1a687d414f5c3dce7bf4a743c" #my.telegram.org
 
-bot_token = ""
+bot_token = "6845914467:AAGqfwYU5vZdRz-k5Fs6fhuXYJm_RH5E9tY"
 
 CHAT_ID = -100 #chatid and make sure add bot on this chat and promote as admin 
 
 
 cookies = {
+    'browserid': 'PutoLSrikIu91W1REHoJLEEhJsjNp2HQlBpl2bvbmjTSI-kccWC7TvuxJ88=',
+    'lang': 'en',
+    '__bid_n': '18ce2a5d2f4cd311ec4207',
+    '_ga': 'GA1.1.706358978.1704609570',
+    '__stripe_mid': '1231e2b1-ff34-44e0-a973-5801dd84a3a2662c38',
+    'g_state': '{"i_l":0}',
+    'ndus': 'YVTP1CCteHui93gSFn3vtXB7Wd336q-99yFauQI2',
+    'csrfToken': 'DoVWsba_BgSE5e3gp06YtyuG',
+    'ndut_fmt': '1A6BDBD3461537587826982D43F1FBE8430682F1D9981DB2781B86C78E49B87F',
+    '_ga_06ZNKL8C2E': 'GS1.1.1704616974.3.0.1704616984.50.0.0',
 }
 
-headers = {}
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'browserid=PutoLSrikIu91W1REHoJLEEhJsjNp2HQlBpl2bvbmjTSI-kccWC7TvuxJ88=; lang=en; __bid_n=18ce2a5d2f4cd311ec4207; _ga=GA1.1.706358978.1704609570; __stripe_mid=1231e2b1-ff34-44e0-a973-5801dd84a3a2662c38; g_state={"i_l":0}; ndus=YVTP1CCteHui93gSFn3vtXB7Wd336q-99yFauQI2; csrfToken=DoVWsba_BgSE5e3gp06YtyuG; ndut_fmt=1A6BDBD3461537587826982D43F1FBE8430682F1D9981DB2781B86C78E49B87F; _ga_06ZNKL8C2E=GS1.1.1704616974.3.0.1704616984.50.0.0',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
